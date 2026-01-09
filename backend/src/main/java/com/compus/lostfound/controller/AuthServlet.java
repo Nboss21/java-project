b8835c1330345e2313dfcc88f42c7e92494fe4d0
Rebira Adugna <rebikman9@gmail.com>
2026-01-09 16:32:49 +0300

    Add webServlet authentication to AuthServlet
    Added webServlet authentication for AuthServlet.

--- backend/src/main/java/com/compus/lostfound/controller/AuthServlet.java
+++ backend/src/main/java/com/compus/lostfound/controller/AuthServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
+// Add webServlet authentication 
 @WebServlet("/api/auth/*")
 public class AuthServlet extends HttpServlet {
     private UserDAO userDAO;
